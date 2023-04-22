2de3e914d01fff9b981ae194dff8380fcb7477b2
Darren Gomes <darreng18.me@gmail.com>
2023-04-23 01:38:51 +0400

    updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import { createClient } from "redis";
+import createClient from "redis";
 import express from 'express';
-import Mongo,{ MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 let app = express();
+const port = process.env.PORT ||8100
 const url = "mongodb://localhost:27017";
 const mClient = new MongoClient(url);
 const rClient = createClient({host: 'localhost', port:6379});
@@ -14,12 +15,12 @@ async function main(){
     await mClient.connect();
 }
 
-
+const collection = mClient.db('node').collection('products')
 
 app.get('/data', async(req,res)=>{
     await rClient.connect()
     let uInput = req.query.color.trim();
-    let result = await clientInformation.get(uInput);
+    let result = await rClient.get(uInput);
     if(result){
         const output = JSON.parse(result);
         res.send(output);
@@ -27,6 +28,18 @@ app.get('/data', async(req,res)=>{
         // if not in redis, from mongo
         const output = [];
         let query = {Color:uInput} 
-        const data = 
+        const cursor = collection.find({Color:uInput});
+        for await(const data of cursor){
+            output.push(data);
+        }
+        await ClientSession.set(uInput, JSON.stringify({source:"Redis cache", output}),{EX:12, NX:true}); 
+        cursor.closed;
+        res.send({source:"MongoDB", output});
     }
+    await rClient.disconnect();
+})
+
+app.listen(port,(err)=>{
+    main();
+    console.log(`Server is running on port ${port}`);
 })
